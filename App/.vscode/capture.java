d0f26c088de56b5afab6d2f62f002c2df2dbd1d1
adeelakmal <adeelakmal0@gmail.com>
2023-04-01 20:44:08 +0500

    server working
    server is now able to establish connection with the java application. Changes have been made in capture.java accordingly

--- App/.vscode/capture.java
+++ App/.vscode/capture.java
@@ -5,129 +5,131 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.application.Platform;
-// import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 import java.io.ByteArrayInputStream;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
 import org.opencv.videoio.VideoCapture;
 import org.opencv.imgcodecs.Imgcodecs;
 
+import java.net.*;
+import java.io.*;
+
 public class capture extends Application {
-    // Start the video capture thread
 
-    class CameraThread extends Thread {
+    // IP/index/filename of the cameras
+    static String[] sources = {
+            "E:/Uni Assignments/SEMESTER 7/COMP 497A/DATASET/rebuilt.Anomaly-Videos-Part-3/Anomaly-Videos-Part-3/Robbery/Robbery001_x264.mp4",
+            "E:/Uni Assignments/SEMESTER 7/COMP 497A/DATASET/rebuilt.Anomaly-Videos-Part-3/Anomaly-Videos-Part-3/Robbery/Robbery002_x264.mp4",
+            "E:/Uni Assignments/SEMESTER 7/COMP 497A/DATASET/rebuilt.Anomaly-Videos-Part-3/Anomaly-Videos-Part-3/Robbery/Robbery003_x264.mp4",
+            "E:/Uni Assignments/SEMESTER 7/COMP 497A/DATASET/rebuilt.Anomaly-Videos-Part-3/Anomaly-Videos-Part-3/Robbery/Robbery004_x264.mp4",
+            "E:/Uni Assignments/SEMESTER 7/COMP 497A/DATASET/rebuilt.Anomaly-Videos-Part-3/Anomaly-Videos-Part-3/Robbery/Robbery015_x264.mp4",
+            "E:/Uni Assignments/SEMESTER 7/COMP 497A/DATASET/rebuilt.Anomaly-Videos-Part-3/Anomaly-Videos-Part-3/Robbery/Robbery016_x264.mp4",
+            "E:/Uni Assignments/SEMESTER 7/COMP 497A/DATASET/rebuilt.Anomaly-Videos-Part-3/Anomaly-Videos-Part-3/Robbery/Robbery017_x264.mp4",
+            "E:/Uni Assignments/SEMESTER 7/COMP 497A/DATASET/rebuilt.Anomaly-Videos-Part-3/Anomaly-Videos-Part-3/Robbery/Robbery018_x264.mp4",
+            "E:/Uni Assignments/SEMESTER 7/COMP 497A/DATASET/rebuilt.Anomaly-Videos-Part-3/Anomaly-Videos-Part-3/Robbery/Robbery019_x264.mp4" };
+    // Creating Imageviews and captures for the frames to be captured
+    VideoCapture[] captures = new VideoCapture[sources.length];
+    ImageView[] imageView = new ImageView[sources.length];
+    Rectangle[] borders = new Rectangle[sources.length];
 
-        private volatile VideoCapture capture;
-        private volatile Mat frame;
-        private volatile ImageView imageView;
-        private volatile int ThreadId;
+    private static final int NUM_SOURCES = sources.length; // Number of video sources
+    private static final int FRAME_RATE = 25; // Frame rate of the video
 
-        public CameraThread(int capture, ImageView imageView, int ThreadId) {
-            this.capture = new VideoCapture(capture);
-            this.frame = new Mat();
-            this.imageView = imageView;
-            this.ThreadId = ThreadId;
+    // Executor to create seperate threads for frame capture
+    private static final ScheduledExecutorService executor = Executors.newScheduledThreadPool(NUM_SOURCES);
 
-        }
+    // Maybe remove this function because it is causing issues in frame capture
+    private Image convertMatToImage(Mat mat) {
+        // Convert the Mat object to a byte array
+        MatOfByte buffer = new MatOfByte();
+        Imgcodecs.imencode(".png", mat, buffer);
 
-        public CameraThread(String capture, ImageView imageView, int ThreadId) {
-            this.capture = new VideoCapture(capture);
-            this.frame = new Mat();
-            this.imageView = imageView;
-            this.ThreadId = ThreadId;
-        }
+        // Convert the byte array to a JavaFX image
+        Image image = new Image(new ByteArrayInputStream(buffer.toArray()));
 
-        @Override
-        public void run() {
-
-            while (true) {
-                synchronized (this) {
-                    frame = new Mat();
-                    if (capture.read(frame)) {
-
-                        // Convert the OpenCV frame to a JavaFX image
-                        Image image = convertMatToImage(frame);
-
-                        // Display the image in the ImageView
-                        // imageView.setImage(image);
-                        Platform.runLater(() -> imageView.setImage(image));
-                    } else {
-                        System.err.println("Error: could not read frame from capture device.");
-                        break;
-                    }
-
-                }
-            }
-            capture.release();
-
-        }
-
-        // Maybe remove this function because it is causing issues in frame capture
-        private Image convertMatToImage(Mat mat) {
-            // Convert the Mat object to a byte array
-            MatOfByte buffer = new MatOfByte();
-            Imgcodecs.imencode(".png", mat, buffer);
-            // Convert the byte array to a JavaFX image
-            Image image = new Image(new ByteArrayInputStream(buffer.toArray()));
-
-            return image;
-        }
-
+        return image;
     }
 
     @Override
     public void start(Stage stage) {
+        try {
 
+            // To establish connection with Python server
+            String serverName = "127.0.0.1";
+            int port = 8000;
+
+            Socket clientSocket = new Socket(serverName, port);
+            OutputStream outputStream = clientSocket.getOutputStream();
+            InputStream inputStream = clientSocket.getInputStream();
+
+            // Sending a message to server
+            String message = "Hello from the client!";
+            outputStream.write(message.getBytes());
+
+            clientSocket.close();
+            outputStream.close();
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         GridPane grid = new GridPane();
         grid.setPadding(new Insets(10));
         grid.setHgap(10);
         grid.setVgap(10);
-        // IP/index/filename of the cameras
-        String[] captures = {
-                "E:/Uni Assignments/SEMESTER 7/COMP 497A/DATASET/rebuilt.Anomaly-Videos-Part-3/Anomaly-Videos-Part-3/Robbery/Robbery001_x264.mp4",
-                "E:/Uni Assignments/SEMESTER 7/COMP 497A/DATASET/rebuilt.Anomaly-Videos-Part-3/Anomaly-Videos-Part-3/Robbery/Robbery002_x264.mp4",
-                "E:/Uni Assignments/SEMESTER 7/COMP 497A/DATASET/rebuilt.Anomaly-Videos-Part-3/Anomaly-Videos-Part-3/Robbery/Robbery003_x264.mp4",
-                "E:/Uni Assignments/SEMESTER 7/COMP 497A/DATASET/rebuilt.Anomaly-Videos-Part-3/Anomaly-Videos-Part-3/Robbery/Robbery004_x264.mp4",
-                "E:/Uni Assignments/SEMESTER 7/COMP 497A/DATASET/rebuilt.Anomaly-Videos-Part-3/Anomaly-Videos-Part-3/Robbery/Robbery005_x264.mp4",
-                "E:/Uni Assignments/SEMESTER 7/COMP 497A/DATASET/rebuilt.Anomaly-Videos-Part-3/Anomaly-Videos-Part-3/Robbery/Robbery006_x264.mp4",
-                "E:/Uni Assignments/SEMESTER 7/COMP 497A/DATASET/rebuilt.Anomaly-Videos-Part-3/Anomaly-Videos-Part-3/Robbery/Robbery007_x264.mp4",
-                "E:/Uni Assignments/SEMESTER 7/COMP 497A/DATASET/rebuilt.Anomaly-Videos-Part-3/Anomaly-Videos-Part-3/Robbery/Robbery008_x264.mp4",
-                "E:/Uni Assignments/SEMESTER 7/COMP 497A/DATASET/rebuilt.Anomaly-Videos-Part-3/Anomaly-Videos-Part-3/Robbery/Robbery009_x264.mp4" };
 
-        // Creating Image views for the frames to be captured
-        ImageView[] imageView = new ImageView[captures.length];
-        Rectangle[] borders = new Rectangle[captures.length];
-        for (int i = 0; i < captures.length; i++) {
+        // VideoCapture[] captures = new VideoCapture[sources.length];
+        for (int i = 0; i < sources.length; i++) {
+            captures[i] = new VideoCapture(sources[i]);
+        }
+
+        for (int i = 0; i < sources.length; i++) {
             ImageView view = new ImageView();
             view.setFitHeight(144);
             view.setFitWidth(256);
 
             Rectangle border = new Rectangle(256, 144, Color.TRANSPARENT);
-            border.setStroke(Color.RED);
+            border.setStroke(Color.BLUE);
             border.setStrokeWidth(2);
 
             imageView[i] = view;
             borders[i] = border;
         }
 
-        Thread[] threads = new CameraThread[captures.length];
-        for (int i = 0; i < captures.length; i++) {
-            threads[i] = new CameraThread(captures[i], imageView[i], i);
-            threads[i].setDaemon(true);
-            threads[i].start();
+        // Start the video capture threads
+        for (int i = 0; i < NUM_SOURCES; i++) {
+            final int index = i;
+            executor.scheduleAtFixedRate(() -> {
+                // Capture the frame
+                Mat frame = new Mat(640, 480, CvType.CV_8UC3);
+                if (captures[index].read(frame)) {
+
+                    // System.out.println(frame.get(10, 10)[0]);
+
+                    // Convert the frame to an image
+                    Image image = convertMatToImage(frame);
+
+                    // Update the image view
+                    Platform.runLater(() -> imageView[index].setImage(image));
+                }
+
+            }, 0, 1000 / FRAME_RATE, TimeUnit.MILLISECONDS);
         }
 
         stage.setTitle("Video Capture");
 
         // Putting all the ImageViews inside a 3x3 grid by checking the total amount in
         // the captures array
-        int TOTAL_CAMERAS = captures.length;
+        int TOTAL_CAMERAS = sources.length;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if (TOTAL_CAMERAS > 0) {
@@ -143,17 +145,24 @@ public class capture extends Application {
         Scene scene = new Scene(grid, 820, 480);
         stage.setScene(scene);
         stage.show();
+    }
 
-        // try {
-        // for (int i = 0; i < captures.length; i++) {
-        // threads[i].join();
-        // }
-        // } catch (InterruptedException e) {
-        // e.printStackTrace();
-        // }
+    @Override
+    public void stop() throws Exception {
+        // Stop the video capture threads
+        executor.shutdownNow();
+
+        // Release the video captures
+        for (VideoCapture capture : captures) {
+            capture.release();
+        }
+
+        // Shutdown OpenCV
+        System.exit(0);
     }
 
     public static void main(String args[]) {
+
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         launch(args);
 
--- server.py
+++ server.py
@@ -1,8 +1,33 @@
 import cv2
+import time
+import socket
+
+# To establush connection with client
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# host = socket.gethostname()
+host = "127.0.0.1"
+port = 8000
+server_socket.bind((host, port))
+server_socket.listen(1)
+print(host)
+
 video = cv2.VideoCapture(0)
 waitTime = 0.5
 frameCount = 0
 collectedFrames = []
+
+print("Waiting for connection...")
+client_socket, addr = server_socket.accept()
+print(f"Connection from {addr} has been established!")
+data = client_socket.recv(1024)
+print(f"Received data from client: {data.decode()}")
+
+
+def DummyWait(collected):
+    time.sleep(5)
+    return True
+
+
 while True:
     ret, frame = video.read()
     if not ret:
@@ -13,10 +38,12 @@ while True:
         collectedFrames.append(frame)
     if len(collectedFrames) == 100:
         print(f"Time Taken: {waitTime * 100} seconds", )
+        result = DummyWait(collectedFrames)
+
         print(len(collectedFrames))
         collectedFrames = []
-# if cv2.waitKey(1) & 0xFF == ord('q'):
-#     break
+
 
 video.release()
+client_socket.close()
 cv2.destroyAllWindows()
