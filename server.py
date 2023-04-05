28a1f64761c9ab4c78f23ea7a0da9181721e9d0f
adeelakmal <adeelakmal0@gmail.com>
2023-04-05 21:09:58 +0500

    integrating model
    model has been integrated into the server

--- server.py
+++ server.py
@@ -1,6 +1,16 @@
 import cv2
 import time
 import socket
+import tensorflow as tf
+import numpy as np
+
+# Import saved model
+new_model = tf.keras.models.load_model('model1.h5')
+
+# Possible classes
+CLASSES = ["Robbery", "Burglary", "Normal", "Fighting"]
+print(new_model.summary())
+
 
 # To establush connection with client
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -16,31 +26,37 @@ waitTime = 0.5
 frameCount = 0
 collectedFrames = []
 
-print("Waiting for connection...")
-client_socket, addr = server_socket.accept()
-print(f"Connection from {addr} has been established!")
-data = client_socket.recv(1024)
-print(f"Received data from client: {data.decode()}")
+# print("Waiting for connection...")
+# client_socket, addr = server_socket.accept()
+# print(f"Connection from {addr} has been established!")
+# data = client_socket.recv(1024)
+# print(f"Received data from client: {data.decode()}")
 
 
-def DummyWait(collected):
-    time.sleep(5)
-    return True
+def getPrediction(collected):
+    print(f"Input shape is: {np.asarray(collected).shape} ")
+    a = []
+    a.append(collected)
+    print(np.asarray(a).shape)
+    return new_model.predict(np.asarray(a))
 
 
 while True:
     ret, frame = video.read()
+    resized_frame = cv2.resize(frame, (64, 64))
+    normalized_frame = resized_frame / 255
     if not ret:
         print("frame count: ", frameCount)
         break
     frameCount += 1
     if frameCount % (waitTime * video.get(cv2.CAP_PROP_FPS)):
-        collectedFrames.append(frame)
+        collectedFrames.append(normalized_frame)
     if len(collectedFrames) == 100:
         print(f"Time Taken: {waitTime * 100} seconds", )
-        result = DummyWait(collectedFrames)
+        result = getPrediction(collectedFrames)
 
-        print(len(collectedFrames))
+        print(np.argmax(result))
+        print(CLASSES[np.argmax(result)])
         collectedFrames = []
 
 
